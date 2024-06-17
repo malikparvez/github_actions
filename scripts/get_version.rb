8fc60aa4c83716d9d9c9fec9992a81a66de56566
Malik Parvez <84777619+malikparvez@users.noreply.github.com>
2024-06-17 23:16:14 +0530

    Create get_version.rb

---
+++ scripts/get_version.rb
@@ -0,0 +1,13 @@
+require 'json'
+
+# Read metadata.json
+version = JSON.parse(File.read('metadata.json'))['version']
+
+# Version string matching regex
+semver_regex = /\A(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?\z/
+
+if version&.match?(semver_regex)
+  puts "version=#{version}"
+else
+  raise "Version #{version} is invalid. Exiting workflow."
+end
